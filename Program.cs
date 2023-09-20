6011876a97c633e9395753361331c26f522e981b
TatendaBeta <tatsbeta@gmail.com>
2023-09-20 16:20:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_and_Func_Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
